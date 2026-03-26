e47e60829e9c57fc7f4f9c965006b08ddc60b709
pankajgodham <168950256+pankajgodham@users.noreply.github.com>
2026-03-26 14:14:57 +0530

    Full_Name field

--- SqlDatabaseProject/dbo/Tables/Users.sql
+++ SqlDatabaseProject/dbo/Tables/Users.sql
@@ -1,13 +1,14 @@
 ﻿CREATE TABLE [dbo].[Users] (
-    [UserId]       INT            IDENTITY (1, 1) NOT NULL,
-    [Username]     NVARCHAR (100) NULL,
-    [Password]     NVARCHAR (200) NULL,
-    [RoleId]       INT            NULL,
-    [Phone]        NVARCHAR (15)  NULL,
-    [Email]        NVARCHAR (15)  NULL,
-    [User_Address] NVARCHAR (15)  NULL,
-    [User_pincode] NVARCHAR (15)  NULL,
-    [User_State]   NVARCHAR (15)  NULL,
+    [UserId]        INT            IDENTITY (1, 1) NOT NULL,
+    [Username]      NVARCHAR (100) NULL,
+    [Password]      NVARCHAR (200) NULL,
+    [RoleId]        INT            NULL,
+    [Phone]         NVARCHAR (15)  NULL,
+    [Email]         NVARCHAR (15)  NULL,
+    [User_Address]  NVARCHAR (15)  NULL,
+    [User_pincode]  NVARCHAR (15)  NULL,
+    [User_State]    NVARCHAR (15)  NULL,
+    [User_fullName] NVARCHAR (15)  NULL,
     PRIMARY KEY CLUSTERED ([UserId] ASC),
     FOREIGN KEY ([RoleId]) REFERENCES [dbo].[Roles] ([RoleId])
 );
